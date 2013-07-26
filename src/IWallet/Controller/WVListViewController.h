31d338729e60711d7227f9d23b3e89ea81961765
张 锋 <zhangfeng0408@gmail.com>
2013-07-26 23:22:29 +0800

    tried drawer effect

---
+++ src/IWallet/Controller/WVListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WVListViewController.h
+//  IWallet
+//
+//  Created by 张 锋 on 7/26/13.
+//  Copyright (c) 2013 Windvoice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WVListViewController : UIViewController
+
+@end
---
+++ src/IWallet/Controller/WVListViewController.m
@@ -0,0 +1,38 @@
+//
+//  WVListViewController.m
+//  IWallet
+//
+//  Created by 张 锋 on 7/26/13.
+//  Copyright (c) 2013 Windvoice. All rights reserved.
+//
+
+#import "WVListViewController.h"
+
+@interface WVListViewController ()
+
+@end
+
+@implementation WVListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
